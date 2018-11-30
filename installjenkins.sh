66a4f5d65c9f8879156bbc47d9285de5637b3cf2
jayabalan <jayabalanbala@gmail.com>
2018-11-29 22:24:49 -0600

    Jenkins test temp fix

---
+++ installjenkins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+yum remove -y java
+yum install -y java-1.8.0-openjdk wget
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install -y jenkins
+service jenkins start
+chkconfig jenkins on
+
+echo "Install Telegraf"
+wget https://dl.influxdata.com/telegraf/releases/telegraf-1.6.0-1.x86_64.rpm -O /tmp/telegraf.rpm
+yum localinstall -y /tmp/telegraf.rpm
+rm /tmp/telegraf.rpm
+chkconfig telegraf on
+mv /tmp/telegraf.conf /etc/telegraf/telegraf.conf
+service telegraf start
+
+echo "Install git"
+yum install -y git
+
